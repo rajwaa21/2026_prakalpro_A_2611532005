e72fdbc378738a87ba170c30af7e907a29937ef4
rajwa hasna <rajwahasna218@gmail.com>
2026-09-09 10:24:21 +0700

    pekan2
    Signed-off-by: rajwa hasna <rajwahasna218@gmail.com>

---
+++ pekan2/String_2611532005.py
@@ -0,0 +1,12 @@
+nama_2005 = "Rajwa Hasna Khairiyah"
+jenis_kelamin_2005 = 'P'
+alamat_2005 = """
+    Komplek wisma bumi mas
+    Kec.Kuranji
+    Kota Padang
+"""
+agama_2005 = "Islam"
+print("Nama saya adalah:", nama_2005)
+print("Jenis kelamin saya adalah:", jenis_kelamin_2005)
+print("Alamat saya adalah:", alamat_2005)
+print("Agama saya adalah:", agama_2005)
